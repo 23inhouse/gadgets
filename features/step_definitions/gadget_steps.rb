1da6ec77a75f859e58c0e240fb1d2393ef79d718
Benjamin Lewis <23inhouse@gmail.com>
2014-08-12 17:05:45 +0930

    add the pending gadget steps

---
+++ features/step_definitions/gadget_steps.rb
@@ -0,0 +1,7 @@
+When(/^I add a gadget$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I edit a gadget$/) do
+  pending # express the regexp above with the code you wish you had
+end
